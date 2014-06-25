17d3cad984bc57bc8751215ffdce50fbb5c0d28d
SpencerTranter <spencertranter@Spencers-MacBook-Pro.local>
2014-06-25 10:38:45 -0700

    First commit, needs to move past first question

---
+++ script.js
@@ -0,0 +1,7 @@
+var userAge=parseInt(prompt('What is your age?'), 10)
+  if (userAge===isNaN) {
+    console.log('Sorry you did not seem to submit anything.') }
+  else if(userAge<13) {
+    console.log('Sorry you must be 13 or older!') }
+  else {
+    console.log('Great, right this way please') }
